ec4ffbcaa7ff1eb0a33a0945a88fc2beed85bbe4
Marcin Tokarczyk <marcintokarczyk37@gmail.com>
2024-12-08 12:42:23 +0000

    Added extracting title + saving output file in output folder with title as a name

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import yt_dlp
 
-url = r"https://www.youtube.com/watch?v=0hpgMPkfsec"  # Link do wideo na YouTube
 
-# Opcje dla yt-dlp
+# Settings
+
+# url adress of youtube video
+url = r"https://www.youtube.com/watch?v=0hpgMPkfsec"
+
+# yt-dlp options
 ydl_opts = {
     'format': 'bestaudio/best',  # Pobranie najlepszego strumienia audio
     'postprocessors': [
@@ -13,13 +17,27 @@ ydl_opts = {
             # 'preferredcodec': 'wav',
             
             # MP3
-            'preferredodec': 'mp3',
+            'preferredcodec': 'mp3',
             'preferredquality': '192'
         }
     ],
     'outtmpl': 'audio',  # Nazwa pliku wynikowego
 }
 
-# Pobieranie
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([url])
+
+def main():
+    # get info about the video
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url=url, download=False)
+        title = info['title']
+    
+    # Set title as name of output file
+    ydl_opts['outtmpl'] = 'output/' + title
+
+    # download audio
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+
+if __name__=="__main__":
+    main()
